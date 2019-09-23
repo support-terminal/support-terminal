fd8f7cea7354a4555372b90ac602bf45b66c6253
SerdukovAA <seaa@1c.ru>
2019-09-23 15:21:07 +0300

    Перепиливаем задачи мониторинга

--- ui/src/app/bot-monitoring/tasks/bot-task-form/bot-task-form.component.ts
+++ ui/src/app/bot-monitoring/tasks/bot-task-form/bot-task-form.component.ts
@@ -1,63 +1,84 @@
-import {Component, EventEmitter, Input, Output, SimpleChanges, ViewEncapsulation} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, Output, SimpleChanges, ViewEncapsulation} from '@angular/core';
 import BotMonitoringTask from "../models/BotMonitoringTask";
 import Notify from "../models/Notify";
+import BotCommand from "../../../bot-commands/commands/models/BotCommand";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {BotCommandsService} from "../../../bot-commands/commands/services/bot-commands.service";
 
 
 @Component({
-    selector: 'bot-task-form',
-    templateUrl: './bot-task-form.component.html',
-    styleUrls: ['./bot-task-form.component.scss'],
-    encapsulation: ViewEncapsulation.Emulated
+  selector: 'bot-task-form',
+  templateUrl: './bot-task-form.component.html',
+  styleUrls: ['./bot-task-form.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class BotTaskFormComponent {
 
-    @Output() onSubmitEvent: EventEmitter<BotMonitoringTask> = new EventEmitter();
-    @Input() initial: BotMonitoringTask;
-    @Input() submitButtonText: string;
+  @Output() onSubmitEvent: EventEmitter<BotMonitoringTask> = new EventEmitter();
+  @Input() initial: BotMonitoringTask;
+  @Input() submitButtonText: string;
 
+  private botTaskModel: BotMonitoringTask = new BotMonitoringTask();
+  private botTaskForm: FormGroup;
 
+  onSubmit(): void {
+    this.botTaskModel = this.botTaskForm.value;
+    this.botTaskModel.action = this.botTaskForm.controls['action'].value;
+    this.onSubmitEvent.emit(this.botTaskModel);
+  }
 
-    private botTask: BotMonitoringTask = new BotMonitoringTask();
+  constructor(private botCommandsService: BotCommandsService,
+              private fb: FormBuilder) {
 
-    onSubmit(): void {
-        this.onSubmitEvent.emit(this.botTask);
+    this.initForm();
+  }
+
+  initForm() {
+    this.botTaskForm = this.fb.group({
+      name: ['', Validators.required ],
+      notifyList: ['', Validators.required ],
+      botIds: [null, Validators.compose([Validators.minLength(1), Validators.required])],
+      action: this.fb.group({
+        type: [''],
+      })
+    });
+  }
+
+  actionTypes = [
+    {name: 'Выборка единичного числового значения из базы данных', type: 'SQL_SELECT_ONE_NUMERIC_VALUE'},
+  ];
+
+  cronFrequencies = [
+    {name: 'Каждую минут', type: 'EVERY_1_MINUTE'},
+    {name: 'Каждые 5 минут', type: 'EVERY_5_MINUTES'},
+    {name: 'Каждые 15 минут', type: 'EVERY_15_MINUTES'},
+    {name: 'Каждый час', type: 'EVERY_HOUR'}
+  ];
+
+  notifyTypes = [
+    {name: 'Уведомить через Slack', type: 'SLACK_BOT_API'}
+  ];
+
+  ngOnChanges(changes: SimpleChanges) {
+/*    if (changes['initial']) {
+      if (this.initial != null && this.initial != undefined) {
+        this.botTask = this.initial;
+      }
+    }*/
+  }
+
+  addNotify(type: string) {
+    if (!Array.isArray(this.botTaskForm.value.notifyList)) {
+      this.botTaskForm.notifyList = new Array();
     }
+    this.botTask.notifyList.push(new Notify(type))
+  }
 
-    actionTypes = [
-        {name: 'Выборка единичного числового значения из базы данных', type: 'SQL_SELECT_ONE_NUMERIC_VALUE'},
-    ];
-
-    cronTypes = [
-        {name: 'Каждую минут', type: 'EVERY_1_MINUTE'},
-        {name: 'Каждые 5 минут', type: 'EVERY_5_MINUTES'},
-        {name: 'Каждые 15 минут', type: 'EVERY_15_MINUTES'},
-        {name: 'Каждый час', type: 'EVERY_HOUR'}
-    ];
-
-    notifyTypes = [
-        {name: 'Уведомить через Slack', type: 'SLACK_BOT_API'}
-    ];
-
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes['initial']) {
-            if (this.initial != null && this.initial != undefined) {
-                this.botTask = this.initial;
-            }
-        }
-    }
-
-    addNotify(type: string){
-        if (!Array.isArray(this.botTask.notifyList)) {
-            this.botTask.notifyList = new Array();
-        }
-        this.botTask.notifyList.push(new Notify(type))
-    }
-
-    dropNotify(index:number) {
-        if (Array.isArray(this.botTask.notifyList)) {
-            this.botTask.notifyList.splice(index, 1);
-        }
-    }
+  dropNotify(index: number) {
+/*    if (Array.isArray(this.botTask.notifyList)) {
+      this.botTask.notifyList.splice(index, 1);
+    }*/
+  }
 
 
 }
